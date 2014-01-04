3385d6bf7cee5a3fa0099e8d4bc6116000f9b21e
Matthew McGovern <mmcgover@oswego.edu>
2014-01-04 11:55:53 -0500

    12/3/2014 – 12/4/2013:
    Removed unnecessary variables and old/unnecessary/redundant lines of code from demo3 and EventHandler. Started working on better documentation for both files.
    
    Continued to work on documentation for demo3. Changed the way it prints the demo prints the data at the end. Data is now printed on one line and prints the stimuli used in an orderly manner.
    
    (This may be a redundant commit, but I was worried I might have messed something up with GIT and didn't want to push a bad version to the master. Apologies!)
    
    -mm

---
+++ kelpy/CommandableSprite.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+from pygame import Rect, Color
+from pygame.sprite import Sprite
+from Arrangeable import *
+from Dragable import *
+from time import time
+from copy import copy
+from math import *
+from pygame.locals import *
+import pygame
+
+
+"""
+	TODO: Break this up into separate classes
+	      Change the naming of rotate etc. so that if duration is not specified, it happens immediately
+
+"""
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This extends sprites by making them commandable, so that they will do things at certain times
+# (in a sequence)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class CommandableSprite(Sprite, Dragable):
+	
+	def __init__(self, screen, init_position, isdraggable=False):
+		
+		#Arrangeable.__init__(self)
+		Sprite.__init__(self)
+		
+		Dragable.__init__(self)
+		self.set_draggable(isdraggable)
+		
+		self.commands = []
+		self.running = False	
+		self.screen = screen
+		self.set_position(init_position)
+		self.update_callbacks = [] 
+		self.isdraggable = isdraggable
+		
+	def add_update_callback(self, f):
+		self.update_callbacks.append(f)
+	
+	def update(self):
+		for x in self.update_callbacks:
+			x(self)
+			
+	# this starts the timer for it to start processing actions
+	def start(self):
+		self.running = True
+		self.start_x = self.get_x()
+		self.start_y = self.get_y()
+	
+	def position(self):
+		return [self.get_x(), self.get_y()]
+	
+	def stop(self):
+		self.running = False
+	
+	# This pushes a list of commands onto myself
+	def push_commands(self, **d):
+		self.commands.append(d)
+
+	# get rid of the current command
+	def next_command(self): 
+		if len(self.commands) > 0:
+			del self.commands[0]
+	
+	
+	# NOTE: We re-did this so that each commandable sprite just
+	# processes single commands, and the queue is external
+	# Update the current action
+	def update_action(self, **c):
+
+		if c['first']: # If this is the first function call
+			self.start_x, self.start_y = self.position()
+	
+		action = c['action']
+		duration = c.get('duration', 0.0) # get the duration else 0.0 if none is specified
+		#print duration, duration==0.0
+		t = 1.0 if duration == 0.0 else (time() - c['start_time']) / float(duration) # what percent of the way through are we?
+		
+		# update according to remaining time
+		if action == 'move': # move
+			x,y = c['pos']
+			self.set_x( (x - self.start_x) * t + self.start_x)
+			self.set_y( (y - self.start_y) * t + self.start_y)
+			
+			if c.get('finish',None) is True: self.set_position(c['pos']) # make sure you end up where you should
+		elif action == 'wait': # wait in a location
+			pass # do nothing
+		elif action == 'hide': # hide, don't draw. These implicitly take duration as a "wait" afterwards, NOT as a hide or show for that long
+			self.visible=False
+		elif action == 'show':
+			if self.visible == False: self.last_showtime = time() # store when we were last shown
+			self.visible=True
+		else:
+			# no match
+			return False
+			
+		return True # Matched something 
+		
+	## Overwrite this in inherited classes
+	def draw(self):
+		pass
+	
+	def clickme(self, *args):
+		pass
+	
+	
---
+++ kelpy/Dragable.py
@@ -0,0 +1,128 @@
+
+"""
+	
+"""
+
+from Arrangeable import *
+from Miscellaneous import *
+
+
+class Dragable(Arrangeable):
+	"""
+	A dragable is a kind of arrangeable, but supports drag and drop
+	It also maintains a list of "event zones" which are areas where an event will be
+	triggered if it is drag and/or dropped into
+	"""
+	
+	def __init__(self):
+		Arrangeable.__init__(self)
+	
+		## for drag and drop suport
+		self.clickstart = None  # where were we when the clicking started?
+		self.isdragging = False # are we currently being dragged?
+		self.can_we_drag = True
+		
+		self.drag_zones = []
+		self.drop_zones = []
+		
+	def register_drag_zone(self, z):
+		"""
+			This will make our dragndrop track the location of z, and when we drag onto it, 
+			we post a ZONE_EVENT via process_dragndrop
+			
+			z - any Arrangeable. 
+		"""
+		self.drag_zones.append(z)
+		
+	def register_drop_zone(self, z):
+		"""
+			This will make our dragndrop track the location of z, and when we drag onto it, 
+			we post a ZONE_EVENT via process_dragndrop
+			
+			z - any Arrangeable. 		"""
+		self.drop_zones.append(z)
+	
+	def set_draggable(self,v):
+		if v: self.enable_dragging()
+		else: self.disable_dragging()
+	
+	def disable_dragging(self):
+		"""
+			Prevent drag-n-drop on this
+			This just makes sure we aren't currently dragging
+		"""
+		self.can_we_drag = False
+		self.isdragging = False
+
+	def enable_dragging(self):
+		"""
+			Allow drag-n-drop on this
+		"""
+		self.can_we_drag = True
+		self.isdragging = False
+		
+	def process_dragndrop(self, event):
+		"""
+			Pass this all kelpy events and it handles drag and drop. 
+			
+			When something enters or exits a registered zone, this posts a ZONE_EVENT to pygame.event.Event, with 
+			the following features:
+			motion: drag or drop
+			direction: enter or exit (a zone)
+			obj:  who did the moving
+			zone: what the zone is
+			
+			This then returns true if we are currently dragging. 
+		"""
+		
+		# if we can't even drag, get the hell out of here
+		if self.can_we_drag is False: return False
+		
+		# if there is an event in there
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			if self.click_inside(event.pos):
+				self.isdragging = True
+				self.clickstart = self.position()
+				return True # we are dragging!
+			else:   return False
+			
+		elif self.isdragging:
+			
+			# Process drag
+			if event.type == pygame.MOUSEMOTION:
+				newpos = self.get_x() + event.rel[0] , self.get_y() + event.rel[1] 
+				
+				for z in self.drag_zones:
+					wasinz = z.is_inside( (self.x, self.y) )
+					nowinz = z.is_inside(newpos)
+					if (not wasinz) and nowinz: # the first time you enter
+						pygame.event.post(pygame.event.Event(ZONE_EVENT, motion="drag", direction="enter", obj=self, zone=z))
+					if wasinz and (not nowinz): # first time you exit
+						pygame.event.post(pygame.event.Event(ZONE_EVENT, motion="drag", direction="exit", obj=self, zone=z))
+					
+				self.set_x( newpos[0] )
+				self.set_y( newpos[1] )
+			
+				return True
+
+			# Process drop
+			if event.type == pygame.MOUSEBUTTONUP:
+				self.isdragging=False
+				
+				for z in self.drop_zones:
+					nowinz = z.is_inside( (self.x, self.y) )
+					wasinz = z.is_inside( self.clickstart )
+					
+					if (not wasinz) and nowinz: # drop inside
+						pygame.event.post(pygame.event.Event(ZONE_EVENT, motion="drop", direction="enter", obj=self, zone=z))
+					if wasinz and (not nowinz): # drop outside
+						pygame.event.post(pygame.event.Event(ZONE_EVENT, motion="drop", direction="exit", obj=self, zone=z))
+					
+				
+				return False
+			
+		return False
+		
+	def update(self):
+		Arrangeable.update(self) # call the update
+		
