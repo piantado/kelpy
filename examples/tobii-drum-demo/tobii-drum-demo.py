7005092b76eb1fe213cac221d4e87c06249f0679
Matthew G. McGovern <mmcgover@oswego.edu>
2014-05-07 10:29:32 -0400

    drag and drop changes 	some changes to the inheritance of classes: followable's inherit from dragable's instead of arrangeables. 	this change was made to allow them to throw zone events when being dragged over a drop zone. 	it doesn't seem to be working at this time, but this change should allow more tobii functionality later, since the tobii sprite's inherit from that class.
    There is a new demo, currently non-functional, that uses this new functionality.
    
    some stimuli folders have been renamed to keep the use of _ and - more consistent

--- examples/tobii-drum-demo/tobii-drum-demo.py
+++ examples/tobii-drum-demo/tobii-drum-demo.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 """
-A simple utility for picking out spots on a screen. Running this program will display a gridded, but otherwise blank canvas.
-The will print the coordinates to the console when the user clicks anywhere on the canvas. Voila!
 
+	Eyetracker Demo 2
+	This is a demo that makes use of registered look zones to make some events happen.
 """
 
 import os, sys
@@ -16,23 +16,19 @@ from kelpy.Miscellaneous import *
 from kelpy.DisplayQueue import *
 from kelpy.OrderedUpdates import *
 from kelpy.EventHandler import *
+from kelpy.Dragable import *
+from kelpy.DragDrop import *
+from kelpy.tobii.TobiiSimController import *
+from kelpy.tobii.TobiiSprite import *
 
 IMAGE_SCALE = 0.25
-
 MAX_DISPLAY_TIME = 5.0
 
 ##############################################
 ## Set up pygame
 
-### If there have been no arguments passed to this program from the command line, start at 800,600
-if len(sys.argv) == 1:
-	screen, spot = initialize_kelpy( dimensions=(800,600) )
-else: ## Else we are assuming this has been given two arguments, the length and height.
-	## So we set up using those dimensions. They have to be cast to integers.
-	## Just for fun, note the arguments list is length 3 at this point, 0 is the program name.
+screen, spot = initialize_kelpy( dimensions=(800,600) )
 
-	## Set up using the dimensions from the arguments list.
-	screen, spot = initialize_kelpy( dimensions=(int(sys.argv[1]),int(sys.argv[2])) )
 
 OFF_LEFT = (spot.west)
 
@@ -40,51 +36,63 @@ OFF_LEFT = (spot.west)
 # Run a single trial
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
 
-def present_trial():
+def present_trial(imagepath):
 	"""
 	This is the main function used to run this demo. It is fed an imagepath and uses this to create a CommandableImageSprite offscreen. This Sprite is later moved onto the screen, where it hangs out until it is clicked.
 
 	"""
 	## Images here are commandable sprites, so we can tell them what to do using Q below
-	## We don't actually need this sprite, but I've left it in just so the event loop has something to loop over.
-		
-	array_length = screen.get_width()/20
-	lines_width = [None] * (array_length)
+	tobii_sim = TobiiSimController(screen)
+	img = TobiiSprite( screen, spot.center, imagepath, tobii_sim, scale=IMAGE_SCALE)
 	
-	
+	drum = DropSprite(screen, (609,407), kstimulus("common_objects/drum.png"), scale=IMAGE_SCALE)
 
+	img.register_drag_zone(drum)
 
-	start_time = time()
-	line = pygame.draw.line(screen, (0,0,0), ( spot.center),(screen.get_height(), screen.get_width()), 5)
-	
+	images = [img, drum]
 
 	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 	# Set up the updates, etc. 
+	
 	# A queue of animation operations
 	Q = DisplayQueue()
-
-	dos = OrderedUpdates() # Draw and update in this order
-
+	
+	# Draw a single animation in if you want!
+	Q.append(obj=img, action='wait', duration=1.0)
+	Q.append(obj=img, action='move', pos=spot.center, duration=0.0)
+	
+	# What order do we draw sprites and things in?
+	dos = OrderedUpdates(images) # Draw and update in this order
+	
+	start_time = time()
+	
 	## The standard event loop in kelpy -- this loops infinitely to process interactions
 	## and throws events depending on what the user does
 	for event in kelpy_standard_event_loop(screen, Q, dos, throw_null_events=True):
-		index = 0
-		#numlines= 0
 		
-		
-		if event.type is pygame.MOUSEBUTTONUP:
-			print event.pos
+		img.is_following =  img.is_looked_at()
+		img.process_follow(event)
 
+		if was_dropped_into_zone(event):
+			print "Working somewhat!"
+			play_sound(kstimulus("sounds/Button-Reverb.wav"))
+
+		if( time() - start_time > MAX_DISPLAY_TIME): 
+			pass
 		
+		# If the event is a click:
+		if is_click(event):
+			pass
 	
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Main experiment
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
 
-# Set up images:
 
 
 # present a number of blocks
-for i in range(1):
-	present_trial()
-	
+
+
+
+present_trial(kstimulus("common_objects/glitch_misc/beam.png"))
+		
