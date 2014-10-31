a85fd5330bb6c08831dd9894092d8a335fd57e02
piantado <spiantado@gmail.com>
2014-10-31 16:34:18 -0400

    updates

--- examples/SuperSimpleDemo/demo-drag.py
+++ examples/SuperSimpleDemo/demo-drag.py
@@ -3,7 +3,6 @@
 from kelpy.Miscellaneous import *
 from kelpy.DragDrop import DragSprite
 from kelpy.DisplayQueue import DisplayQueue
-from kelpy.OrderedUpdates import OrderedUpdates
 from kelpy.EventHandler import was_clicked, is_click
 
 screen, locations = initialize_kelpy( dimensions=(800,600) )
@@ -18,7 +17,8 @@ Q.append(obj=myobject, action='move', pos=locations.center, duration=2.0)
 ## and throws events depending on what the user does
 for event in kelpy_standard_event_loop(screen, Q, myobject):
 	
-	myobject.process_dragndrop(event)
+	# If we want to process drag and drops
+	# myobject.process_dragndrop(event)
 	
 	if is_click(event) and not was_clicked(myobject):
 		Q.append(obj=myobject, action='waggle', duration=0.5, amount=1.0, period=0.25)
