30f3850eb1a5309ac7a62affbc6968d5cd9f392b
amandayung <opensource.amanda@gmail.com>
2015-01-19 13:19:39 -0500

    edited conf.py to work with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 
 import sys
 import os
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -104,15 +104,13 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ----------------------------------------------
 
-#readthedocs default theme
-
-html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# readthedocs default theme
+#html_theme = "sphinx_rtd_theme"
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
